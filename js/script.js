3e6f2699945d8f303ee9933ef9927de0d25195ec
Isaac <uniqucode@gmail.com>
2022-06-28 12:57:59 +0100

    Fix a bug

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ const form = document.querySelector('.sign-up__form');
 const feedback = document.querySelector('.feedback');
 const emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 
-
 form.addEventListener('submit', e => {
   e.preventDefault();
 
