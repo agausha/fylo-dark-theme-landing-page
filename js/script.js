b9ab8ac5345b1805fe5e051f8d848f134eccab46
Isaac <uniqucode@gmail.com>
2022-06-28 10:46:07 +0100

    Added emailPattern

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 const form = document.querySelector('sign-up__form');
+const emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
 
 form.addEventListener('submit', e => {
   e.preventDefault();
